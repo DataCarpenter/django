8ce39cbdf2c72b2de81e2fffdd3c0772ddad363d
Laszlo_Kovacs <kovlac.3@gmail.com>
2016-04-27 19:50:26 +0200

    django: message_json file file

--- project/starter_app/views.py
+++ project/starter_app/views.py
@@ -1,11 +1,16 @@
+from config.settings.base import DJANGO_ROOT
 from django.shortcuts import render
-# from .models import Message
+import json
+import os.path
+
+datafile_path=os.path.join(DJANGO_ROOT,"data","messages.json")
+with open(datafile_path) as json_file:
+    json_data = json.load(json_file)
 
 
 def home(request):
-    # messages = Message.objects.order_by('order')
-    messages = ['a-message', 'b-message', 'c-message']
-    context_dict = {
-        'messages': messages
-    }
+    
+    #messages = Message.objects.order_by('order')
+    
+    context_dict = { 'messages': json_data['posts'] }
     return render(request, 'starter_app/home.html', context_dict)
