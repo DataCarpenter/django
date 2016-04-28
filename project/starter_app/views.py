98cf7ffba696a512fe3439981c63717a4f28511c
Laszlo_Kovacs <kovlac.3@gmail.com>
2016-04-28 20:48:33 +0200

    blogpost olvaso
    nem biztos, hogy ez volt a feladat...

--- project/starter_app/views.py
+++ project/starter_app/views.py
@@ -3,14 +3,23 @@ from django.shortcuts import render
 import json
 import os.path
 
-datafile_path=os.path.join(DJANGO_ROOT,"data","messages.json")
+datafile_path = os.path.join(DJANGO_ROOT, "data", "messages.json")
 with open(datafile_path) as json_file:
     json_data = json.load(json_file)
 
 
 def home(request):
-    
+
     #messages = Message.objects.order_by('order')
     
-    context_dict = { 'messages': json_data['posts'] }
+    posts= [x['post'] for x in json_data]
+    context_dict = {'messages': posts}
     return render(request, 'starter_app/home.html', context_dict)
+
+def onepost(request):
+
+    post_name= request.path_info[1:]
+    blogpost = [x for x in json_data if x['post']== post_name]
+
+    context_dict = blogpost[0]
+    return render(request, 'starter_app/post.html', context_dict)
