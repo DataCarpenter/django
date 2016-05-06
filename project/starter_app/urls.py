e8e5d0b876d050faca0ce3690853710d157f6688
Laszlo_Kovacs <kovlac.3@gmail.com>
2016-05-06 20:46:57 +0200

    home button, menu, post teasers, bootstrap

--- project/starter_app/urls.py
+++ project/starter_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^.*_post$', views.onepost,),
+    url(r'^(?P<slug_tag>.+)$', views.onepost,),
 ]
--- project/starter_app/views.py
+++ project/starter_app/views.py
@@ -2,24 +2,47 @@ from config.settings.base import DJANGO_ROOT
 from django.shortcuts import render
 import json
 import os.path
+import datetime
 
 datafile_path = os.path.join(DJANGO_ROOT, "data", "messages.json")
 with open(datafile_path) as json_file:
-    json_data = json.load(json_file)
+    all_message_list = json.load(json_file)
+    all_message_list.sort(key=lambda x: datetime.datetime.strptime(x['date'], '%Y-%m-%d') ,reverse=True)
 
 
 def home(request):
+    message_teaser= get_teasers(all_message_list)
+    context_dict = {'message_details': message_teaser,
+                    'menu': get_menu(),
+                    'actual_slug' : 'home'
+                    }
 
-    #messages = Message.objects.order_by('order')
-    
-    posts= [x['post'] for x in json_data]
-    context_dict = {'messages': posts}
     return render(request, 'starter_app/home.html', context_dict)
 
-def onepost(request):
+def onepost(request, slug_tag):
 
-    post_name= request.path_info[1:]
-    blogpost = [x for x in json_data if x['post']== post_name]
+    blogpost = [x for x in all_message_list if x['slug'] == slug_tag][0]
 
-    context_dict = blogpost[0]
+    context_dict = {'post':blogpost,
+                    'menu':get_menu(),
+                    'actual_slug' : slug_tag}
     return render(request, 'starter_app/post.html', context_dict)
+
+
+
+def get_menu():
+
+    return  [ {'slug':x['slug'],'title': x['title']} for x in all_message_list]
+
+
+def get_teasers(message_list):
+    message_teaser=[]
+    for post in message_list:
+        truncaded_post=dict()
+        for key in post.keys():
+            if key =="content" and len(post[key])>80:
+                truncaded_post[key]=post[key][0:80]
+            else:
+                truncaded_post[key]=post[key]
+        message_teaser.append(truncaded_post)
+    return message_teaser
